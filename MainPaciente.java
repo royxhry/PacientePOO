eaeb2688b732c5d9e9d388486a8730534945f6d0
Rodrigo <hl>
2024-05-02 20:55:22 -0600

    version2

--- MainPaciente.java
+++ MainPaciente.java
@@ -1,26 +1,29 @@
 import java.util.Scanner;
 public class MainPaciente {
-        public static void main(String[] args) {
-        Scanner p1 = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingresa nombre y apellido:");
+            String nombre = sc.nextLine();
+            String apellido = sc.nextLine();
+            System.out.println("Ingresa edad:");
+            int edad = sc.nextInt();
             
-     System.out.println("Ingresa nombre y apellido");
-        String nombre =p1.next();
-        String apellido=p1.next();
-        System.out.println("Ingresa tu edad");
-        int edad=p1.nextInt();
-        System.out.println("Ingresa tu altura");
-        double altura=p1.nextDouble();
-        
-        Paciente p2 = new Paciente();
-        p2.nombre=nombre;
-        p2.apellido= apellido;
-        p2.edad= edad;
-        p2.altura=altura;
-        p2.mostrarNombreA();
-        p2.mostrarEdad();
-        p2.mostrarAtura();
-        p1.close();
-        
+            System.out.println("Ingresa tu altura:");
+            double altura = sc.nextDouble();
+            Paciente ps= new Paciente();
+            ps.setNombre(nombre);
+            ps.setApellido(apellido);
+            ps.setEdad(edad);
+            ps.setAltura(altura);
+
+            ps.getNombre();
+            ps.getApellido();
+            ps.getEdad();
+            ps.getEdad();
+            System.out.println(ps.toString());
+
+            sc.close();
+
     }
 }
 
--- Paciente.java
+++ Paciente.java
@@ -2,19 +2,35 @@ public class Paciente {
     String nombre,apellido;
     int edad;
     double altura;
-    
-    public void mostrarNombreA() {
-        System.out.println("Mi nombre es:"+this.nombre+apellido);
-        
+    public String getNombre() {
+        return nombre;
     }
-    public void mostrarEdad() {
-
-        System.out.println("Mi edad es:"+this.edad);
-
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
     }
-    public void mostrarAtura() {
-
-        System.out.println("Mi altura es:"+this.altura);
+    public String getApellido() {
+        return apellido;
+    }
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+    public int getEdad() {
+        return edad;
+    }
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    public double getAltura() {
+        return altura;
+    }
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+    @Override
+    public String toString() {
+        return "Persona [nombre=" + nombre + ", apellido=" + apellido + ", edad=" + edad + ", altura=" + altura + "]";
+    }
     
-}
+    
+
 }
